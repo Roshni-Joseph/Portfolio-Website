b59e274ff2a83fabfb4b85085c53b4a1768ce9cd
Roshni Joseph <69712886+Roshni-Joseph@users.noreply.github.com>
2020-09-04 11:47:05 +0530

    Portfolio-Website

---
+++ home.js
@@ -0,0 +1,17 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 3000,
+    reset: true
+})
+sr.reveal('.hey',{})
+sr.reveal('.social-icons',{delay:500,origin:'left'})
+sr.reveal('.description',{})
+sr.reveal('.roshimg',{delay:500,origin:'right'})
+sr.reveal('.python-mg',{delay:100,})
+sr.reveal('.html-mg',{delay:200,})
+sr.reveal('.css-mg',{delay:300,})
+sr.reveal('.java-mg',{delay:400,})
+sr.reveal('.js-mg',{delay:500,})
+sr.reveal('.c-mg',{delay:600,})
+sr.reveal('.ps-mg',{delay:700,})
